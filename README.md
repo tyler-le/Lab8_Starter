570c18390902c7bd59ca261ce0fd02d7ea102fad
Tyler Le <tylerle99@yahoo.com>
2022-11-18 17:43:16 -0800

    explore finished

--- README.md
+++ README.md
@@ -1,6 +1,16 @@
 # Lab 8 - Starter
-1) Where would you fit your automated tests in your Recipe project development pipeline? Select one of the following and explain why.
+
+1. Where would you fit your automated tests in your Recipe project development pipeline? Select one of the following and explain why.
+
    - Within a GitHub action that runs whenever code is pushed. You want to incrementally run tests so that, if a failure did occur, you could fix it quickly and easily. This is analogous to pushing a big rock up the hill incrementally as shown in lecture.
 
-2) Would you use an end to end test to check if a function is returning the correct output? (yes/no)
-    - No, E2E testing is used to replicate the workflow from start to finish
+2. Would you use an end to end test to check if a function is returning the correct output? (yes/no)
+
+   - No, E2E testing is used to replicate the workflow from start to finish
+
+3. Would you use a unit test to test the “message” feature of a messaging application? Why or why not? For this question, assume the “message” feature allows a user to write and send a message to another user.
+
+   - No because this functionality can be broken up into multiple units. For example, I would create several unit tests to check if the message can be written, another to check if the message can be sent, and another to test if the message was received. We cannot use unit tests to see how many components interact with eachother.
+
+4. Would you use a unit test to test the “max message length” feature of a messaging application? Why or why not? For this question, assume the “max message length” feature prevents the user from typing more than 80 characters.
+   - Yes, this is an encapsulated unit and isn't dependent on other moving parts of the application.
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -3,3 +3,91 @@
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
 // TODO - Part 2
+
+/* PHONE NUMBER START */
+test('good format should pass', () => {
+    expect(functions.isPhoneNumber("(619)368-8172")).toBe(true);
+});
+
+test('good format should pass', () => {
+    expect(functions.isPhoneNumber("619-368-8172")).toBe(true);
+});
+
+test('letters should fail', () => {
+    expect(functions.isPhoneNumber("(61nine)36eight-8172")).toBe(false);
+});
+test('incorrect format should fail', () => {
+    expect(functions.isPhoneNumber("6193688172")).toBe(false);
+});
+/* PHONE NUMBER END */
+
+
+/* EMAIL START */
+test('good format should pass', () => {
+    expect(functions.isEmail("myemail@gmail.com")).toBe(true);
+});
+
+test('good format should pass', () => {
+    expect(functions.isEmail("111@yahoo.org")).toBe(true);
+});
+
+test('incorrect format should fail', () => {
+    expect(functions.isEmail("tyler.gmailcom")).toBe(false);
+});
+test('incorrect format should fail', () => {
+    expect(functions.isEmail("tyler@yahoo.comm")).toBe(false);
+});
+/* EMAIL END */
+
+/* PASSWORD START */
+test('good format should pass', () => {
+    expect(functions.isStrongPassword("h1234")).toBe(true);
+});
+
+test('good format should pass', () => {
+    expect(functions.isStrongPassword("password")).toBe(true);
+});
+
+test('less than 4 chars', () => {
+    expect(functions.isStrongPassword("abc")).toBe(false);
+});
+test('more than 15 chars', () => {
+    expect(functions.isStrongPassword("abcdefghijklmnopqrstuvwxyz")).toBe(false);
+});
+/* PASSWORD END */
+
+
+/* DATE START */
+test('good format should pass', () => {
+    expect(functions.isDate("01/01/0111")).toBe(true);
+});
+
+test('good format should pass', () => {
+    expect(functions.isDate("08/13/1999")).toBe(true);
+});
+
+test('year as two chars', () => {
+    expect(functions.isDate("08/13/99")).toBe(false);
+});
+test('bad format', () => {
+    expect(functions.isDate("08-13-1999")).toBe(false);
+});
+/* DATE END */
+
+
+/* HEX START */
+test('good format should pass', () => {
+    expect(functions.isHexColor("#333")).toBe(true);
+});
+
+test('good format should pass', () => {
+    expect(functions.isHexColor("#ECECEC")).toBe(true);
+});
+
+test('single pound sign', () => {
+    expect(functions.isHexColor("#")).toBe(false);
+});
+test('too long', () => {
+    expect(functions.isHexColor("#ECECECEC")).toBe(false);
+});
+/* HEX END */
